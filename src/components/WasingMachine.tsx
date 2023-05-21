729050dbdd5c359fc9991d99949a4fc3a66104f5
ploy-thanaporn <tnpsp1501@gmail.com>
2023-05-21 23:08:32 +0700

    feat: count down

--- src/components/WasingMachine.tsx
+++ src/components/WasingMachine.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GiWashingMachine } from "react-icons/gi";
 
 import CoinInsertion from "./CoinInsertion";
@@ -20,9 +20,50 @@ const WashingMachineComponent = () => {
     useState<WashingMachine[]>(defaultstate);
 
   const [activeMachineId, setActiveMachineId] = useState<number>(0);
+  const [countdowns, setCountdowns] = useState<{ [key: number]: number }>({});
+  const [notifications, setNotifications] = useState<{ [key: number]: string }>(
+    {}
+  );
 
-  const handleStart = (id: number) => {
+  useEffect(() => {
+    const timers: NodeJS.Timeout[] = [];
+    Object.keys(countdowns).forEach((machineId) => {
+      const id = Number(machineId);
+      const countdown = countdowns[id];
+      if (countdown > 0) {
+        const timer = setTimeout(() => {
+          setCountdowns((prevCountdowns) => ({
+            ...prevCountdowns,
+            [id]: prevCountdowns[id] - 1,
+          }));
+        }, 1000);
+        timers.push(timer);
+      } else {
+        const activeMachine = washingMachines.find(
+          (machine) => machine.id === id
+        );
+        if (activeMachine) {
+          setNotifications((prevNotifications) => ({
+            ...prevNotifications,
+            [id]: `Washing machine ${activeMachine.model} finished`,
+          }));
+          setActiveMachineId(0);
+        }
+      }
+    });
+
+    return () => {
+      timers.forEach((timer) => clearTimeout(timer));
+    };
+  }, [countdowns, washingMachines]);
+
+  const handleStart = (id: number, model: string) => {
     setActiveMachineId(id);
+    setCountdowns((prevCountdowns) => ({
+      ...prevCountdowns,
+      // Set the countdown to 60 seconds (1 minute)
+      [id]: 60,
+    }));
   };
 
   const handleCoinInserted = (machineId: number) => {
@@ -55,7 +96,7 @@ const WashingMachineComponent = () => {
                       : "bg-blue-500"
                   }`}
                   disabled={machine.availability === false}
-                  onClick={() => handleStart(machine.id)}
+                  onClick={() => handleStart(machine.id, machine.model)}
                 >
                   Start
                 </button>
@@ -64,6 +105,15 @@ const WashingMachineComponent = () => {
                 onCoinInserted={() => handleCoinInserted(machine.id)}
                 isCoinInserted={machine.availability}
               />
+            </div>
+            <div>
+              {!notifications[machine.id] ? (
+                <p>{countdowns[machine.id] ?? 0}</p>
+              ) : (
+                <div className="mt-4 text-center">
+                  <p>{notifications[machine.id]}</p>
+                </div>
+              )}
             </div>
           </div>
         ))}
